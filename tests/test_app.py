15297663bf37819e34e829948855ecd4e560ea2e
Mrinali <dimple.k.chaudhari@gmail.com>
2025-04-28 08:58:20 -0700

    mew code added

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,6 +8,6 @@ def test_add():
 def test_multiply():
     assert multiply(3, 4) == 12
 
-def test_multiply():
-    assert multiply(3, 4) == 11  # This will FAIL!
+def testmultiply():
+    assert multiply(2, 6) == 12  # This will FAIL!
 
